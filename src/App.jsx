1fe3d167f7c6d2cbadfcadd620caf001524eeb60
carsonwolber <ctw54@cornell.edu>
2025-02-13 01:38:32 -0500

    placeholder added

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>Hi! If you happen to be visiting now, I just created this site and will
+        flesh it out over the next couple of days
       </p>
     </>
   )
