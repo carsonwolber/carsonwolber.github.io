63a1738180a0133f3bc346c7e59465648fbdf444
carsonwolber <ctw54@cornell.edu>
2025-02-13 01:44:32 -0500

    app cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <p>Hi! If you happen to be visiting now, I just created this site and will
