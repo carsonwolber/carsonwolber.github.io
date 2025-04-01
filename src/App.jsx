301e40e7f3b6d1747d5d191df199df470825bd34
carsonwolber <ctw54@cornell.edu>
2025-04-01 02:38:31 -0400

    added page props and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import './App.css'
 import Navbar from './Navbar'
+import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom';
 
 
 function App() {
   return (
-    <>
-      <div className='navbar'>
-        <Navbar />
-      </div>
-      <p>
-        Hi! If you happen to be visiting now, I just created this site and will
-        flesh it out over the next couple of days
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <Navigate replace to={'/about'} />
+        } />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
