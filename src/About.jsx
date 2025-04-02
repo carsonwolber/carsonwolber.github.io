da87643c9ae9bb43dda7b237256e771d03e852fd
carsonwolber <ctw54@cornell.edu>
2025-04-01 20:27:05 -0400

    format improvements

--- src/About.jsx
+++ src/About.jsx
@@ -8,6 +8,24 @@ function About() {
       <div className='navbar'>
         <Navbar curr='About' />
       </div>
+
+      <div className='aboutPageContent'>
+        <div className='portrait'>
+          <img src='/portrait.JPG' />
+        </div>
+        <div className='aboutMainTxt'>
+          <h3>
+            Hello! I am a junior at Cornell University studying Computer Science and
+            Economics. My professional and academic interests are: Compilers, Programming Languages, Operating Systems,
+            International Trade, Behavioral Economics, and Game Theory.
+          </h3>
+        </div>
+        <div className='addAboutTxt'>
+
+        </div>
+
+
+      </div>
     </>)
 }
 
