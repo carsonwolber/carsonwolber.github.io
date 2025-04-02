c79a0f725b809ee256572f6c72e409db6a53df9c
carsonwolber <ctw54@cornell.edu>
2025-04-01 21:20:38 -0400

    filled out about txt

--- src/About.jsx
+++ src/About.jsx
@@ -8,23 +8,27 @@ function About() {
       <div className='navbar'>
         <Navbar curr='About' />
       </div>
-
       <div className='aboutPageContent'>
         <div className='portrait'>
           <img src='/portrait.JPG' />
         </div>
         <div className='aboutMainTxt'>
           <h3>
-            Hello! I am a junior at Cornell University studying Computer Science and
+            Hello! I'm Carson. I am a junior at Cornell University studying Computer Science and
             Economics. My professional and academic interests are: Compilers, Programming Languages, Operating Systems,
             International Trade, Behavioral Economics, and Game Theory.
           </h3>
+          <br />
+          <h3>
+            I'm deeply interested in philosophy, linguistics, geography, and many other things.
+            I try to regularly write about the things that interest me! A currated list of writings is the purpose of the correspondingly named section on this site.
+          </h3>
+          <br />
+          <h3>
+            Lastly, the projects section serves as a more detailed supplement to my resume to show off the various
+            projects I have worked on for classes or personally.
+          </h3>
         </div>
-        <div className='addAboutTxt'>
-
-        </div>
-
-
       </div>
     </>)
 }
